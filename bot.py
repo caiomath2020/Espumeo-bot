12dd7a21029f3459a4e92969b322d3b6e5e7999a
caiomath2020 <caiomath2020@gmail.com>
2026-03-10 10:44:42 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import time
+
+spam_tracker = {}
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,6 +12,28 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f'Bot ligado como {bot.user}')
+    @bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    user = message.author.id
+    now = time.time()
+
+    if user not in spam_tracker:
+        spam_tracker[user] = []
+
+    spam_tracker[user].append(now)
+
+    # manter apenas mensagens dos últimos 5 segundos
+    spam_tracker[user] = [t for t in spam_tracker[user] if now - t < 5]
+
+    if len(spam_tracker[user]) >= 5:
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} pare de spammar.")
+        return
+
+    await bot.process_commands(message)
 
 @bot.command()
 async def ping(ctx):
@@ -19,3 +44,4 @@ async def limpar(ctx, quantidade: int):
 import os
 print(os.getenv("TOKEN"))
 bot.run(os.getenv("TOKEN"))
+
